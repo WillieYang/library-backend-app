219f91457c775d55c8c2544b896aed32a786f4ba
HFY <hfy@WenliYangs-Mac.local>
2020-03-19 20:53:17 +0800

    database url for local/remote

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   url: 'mongodb://localhost:27017/library',
 };
+
+// remote mongodb url: mongodb://34.82.248.74:27017/library
+// local mongodb url: mongodb://localhost:27017/library
