83aa9201562eaadfdc241ea7836dcd841e214b38
Wenli Yang <shengyangsn@yahoo.com>
2020-03-20 13:39:34 +0800

    user cryption

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const environment = process.env.NODE_ENV;
+const stage = require('../../config/env.config')[process.env.NODE_ENV];
 
 const UserSchema = mongoose.Schema({
   name: {
@@ -14,6 +18,23 @@ const UserSchema = mongoose.Schema({
   },
 }, {
   timestamps: true,
+});
+
+UserSchema.pre('save', function save(next) {
+  const user = this;
+  if (!user.isModified('password') || !user.isNew) {
+    next();
+  } else {
+    bcrypt.hash(user.password, stage.saltingRounds, (err, hash) => {
+      if (err) {
+        console.log('Error hasing password for user', user.name);
+        next();
+      } else {
+        user.password = hash;
+        next();
+      }
+    });
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
