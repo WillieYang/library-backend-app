0df2b7098b580e93a5278a72e4428d2d186caa9f
Yang Sheng <sheng.yang@2kchina.com>
2021-07-09 16:27:24 +0800

    remove unnecessary code

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,7 +2,7 @@ const users = require('../controllers/user.controller.js');
 const helpers = require('../../utils/helpers');
 
 module.exports = (app) => {
-  app.post('/users', users.addUser);
+  app.post('/users', helpers.validateToken, users.addUser);
   app.post('/login', users.login);
   app.get('/users', helpers.validateToken, users.getAll);
 };
